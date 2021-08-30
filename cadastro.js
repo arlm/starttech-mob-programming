e710ad3c49f3a089d72883c7fdf0c6216e052c4c
Alexandre Marcondes <alexandre.marcondes@docuware.com>
2021-08-30 19:52:46 +0100

    Cadastro inicial
    Co-authored-by: Clara Vasques <clvasques@users.noreply.github.com>
    Co-authored-by: Gabriela Pandini <gabpandini@users.noreply.github.com>
    Co-authored-by: Paloma <plmsz@users.noreply.github.com>
    Co-authored-by: Gabe Berté <gabrielaberte@users.noreply.github.com>
    Co-authored-by: Rafaela Bittencourt <rafaelabittencourt@users.noreply.github.com>
    Co-authored-by: Laura Ribeiro <ribeirolaura@users.noreply.github.com>
    Co-authored-by: Paloma Calado <palomacalado@yahoo.com.br>
    Co-authored-by: Giu Zambot <giuzambot@gmail.com>
    Co-authored-by: Eudes Rodrigues <eron300@users.noreply.github.com>
    Co-authored-by: Mariana Rufino <mari-rufino-g@users.noreply.github.com>
    Co-authored-by: Jislane Santana <Jislanes@users.noreply.github.com>
    Co-authored-by: Mayhhara F Lilian <mflilian@users.noreply.github.com>
    Co-authored-by: Úrsula Ariel <Ursulariel@users.noreply.github.com>
    Co-authored-by: IzabelaGonzaga <IzabelaGonzaga@users.noreply.github.com>
    Co-authored-by: EmillyRoberta <EmillyRoberta@users.noreply.github.com>
    Co-authored-by: Laura Ribeiro <lauracpribeiro@users.noreply.github.com>
    Co-authored-by: lailacardoso <lailacardoso@users.noreply.github.com>
    Co-authored-by: Luana Kuster <luanadkuster@users.noreply.github.com>

---
+++ cadastro.js
@@ -0,0 +1,156 @@
+export const cadastro = [
+    {
+        nome: "Laila Cardoso",
+        cidade: "Salvador",
+        idade: 19,
+        bebidaFavorita: "Budweiser", 
+        genero: "Feminino",
+        signo: "Virgem"
+    },
+    {
+        nome: "Mariana Rufino",
+        cidade: "São Paulo",
+        idade: 26,
+        bebidaFavorita: "Stella", 
+        genero: "Feminino",
+        signo: "Gêmeos"
+    },
+    {
+        nome: "Izabela Gonzaga",
+        cidade: "Palmas-TO",
+        idade: 24,
+        bebidaFavorita: "Guaraná", 
+        genero: "Feminino",
+        signo: "Capricórnio"
+    },
+    {
+        nome: "Giu Zambot",
+        cidade: "Marília",
+        idade: 41,
+        bebidaFavorita: "Bohemia", 
+        genero: "Feminino",
+        signo: "Sagitário"
+    },
+    {
+        nome: "Cyro Renato",
+        cidade: "Rio de janeiro",
+        idade: 24,
+        bebidaFavorita: "Guaraná ca", 
+        genero: "Masculino",
+        signo: "Virgem"
+    },
+    {
+        nome: "Laurão Ribeiro",
+        cidade: "Belo Horizonte",
+        idade: 26,
+        bebidaFavorita: "Original", 
+        genero: "Feminino",
+        signo: "Escorpião"
+    },
+    {
+        nome: "Mayhhara Morais",
+        cidade: "Carpina/PE",
+        idade: 20,
+        bebidaFavorita: "Vinho",
+        genero: "Feminino",
+        signo: "Gêmeos"
+    },
+    {
+        nome: "Clara Vasques",
+        cidade: "Rio de Janeiro",
+        idade: 25,
+        bebidaFavorita: "Brahma", 
+        genero: "Feminino",
+        signo: "Capricórnio"
+    },
+    {
+        nome: "Paloma Calado",
+        cidade: "Rio de Janeiro",
+        idade: 26,
+        bebidaFavorita: "Guaraná Antartica", 
+        genero: "Feminino",
+        signo: "Aquário"
+    },
+    {
+        nome: "Rafaela Bittencourt",
+        cidade: "Maringá",
+        idade: 23,
+        bebidaFavorita: "cerveja", 
+        genero: "feminino",
+        signo: "virgem"
+    },
+    {
+        nome: "Laura Ribeiro",
+        cidade: "São Paulo",
+        idade: 32,
+        bebidaFavorita: "colorado", 
+        genero: "feminino",
+        signo: "touro",
+    },
+    {
+        nome: "Gabriela Pandini",
+        cidade: "Blumenau",
+        idade: 20,
+        bebidaFavorita: "Patagoia Weisse", 
+        genero: "Feminino",
+        signo: "Peixes",
+    },
+    {
+        nome: "Gabriela Moro",
+        cidade: "Foz do Iguacu",
+        idade: 24,
+        bebidaFavorita: "Refri", 
+        genero: "Feminino",
+        signo: "Virgem"
+    },
+    {
+      nome: "Eudes",
+      cidade: "Mariana",
+      idade: 24,
+      bebidaFavorita: "Skol Beats", 
+      genero: "Masculino",
+      signo: "Touro"
+    },
+    {
+        nome: "Jislane Santana",
+        cidade: "Campina Grande - PB",
+        idade: 26,
+        bebidaFavorita: "Cerveja", 
+        genero: "Feminino",
+        signo: "Gêmeos "
+    },
+    {
+        nome: "Emilly Roberta",
+        cidade: "São Paulo ",
+        idad: 21,
+        bebidaFavorita: "leite com *****", 
+        genero: "Feminino",
+        signo: "Leão"
+    },
+    {
+        nome: "Úrsula Ariel",
+        cidade: "Macapá",
+        idade: 33,
+        bebidaFavorita: "Água", 
+        genero: "Transgênero",
+        signo: "Aquário"
+        
+    },
+    {
+        nome: "Luana Kuster",
+        cidade: "Colatina",
+        idade: 23,
+        bebidaFavorita: "Cerveja", 
+        genero: "Feminino",
+        signo: "Capricórnio"
+    },
+    {
+        nome: "Paloma Souza",
+        cidade: "Salvador",
+        idad: 28,
+        bebidaFavorita: "doBem de coco com chocolate", 
+        genero: "Feminino",
+        signo: "Leão"
+    },
+];
+
