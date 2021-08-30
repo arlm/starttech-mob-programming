ac6baaab3ca2853447062eceae4b97b7cfc5620f
Alexandre Marcondes <alexandre.marcondes@docuware.com>
2021-08-30 21:36:43 +0100

    Acrescentando mais detalhes nos testes
    Co-authored-by: Rafaela Bittencourt <rafaelabittencourt@users.noreply.github.com>
    Co-authored-by: Clara Vasques <clvasques@users.noreply.github.com>
    Co-authored-by: Gabe Berté <gabrielaberte@users.noreply.github.com>
    Co-authored-by: Laura Ribeiro <ribeirolaura@users.noreply.github.com>
    Co-authored-by: Gabriela Pandini <gabpandini@users.noreply.github.com>
    Co-authored-by: Giu Zambot <giuzambot@gmail.com>
    Co-authored-by: IzabelaGonzaga <IzabelaGonzaga@users.noreply.github.com>
    Co-authored-by: Paloma <plmsz@users.noreply.github.com>
    Co-authored-by: Laura Ribeiro <lauracpribeiro@users.noreply.github.com>
    Co-authored-by: Eudes Rodrigues <eron300@users.noreply.github.com>
    Co-authored-by: Mariana Rufino <mari-rufino-g@users.noreply.github.com>
    Co-authored-by: Mayhhara F Lilian <mflilian@users.noreply.github.com>
    Co-authored-by: Úrsula Ariel <Ursulariel@users.noreply.github.com>
    Co-authored-by: Luana Kuster <luanadkuster@users.noreply.github.com>
    Co-authored-by: Paloma Calado <palomacalado@yahoo.com.br>
    Co-authored-by: EmillyRoberta <EmillyRoberta@users.noreply.github.com>
    Co-authored-by: Jislane Santana <Jislanes@users.noreply.github.com>
    Co-authored-by: lailacardoso <lailacardoso@users.noreply.github.com>

--- cadastro.spec.js
+++ cadastro.spec.js
@@ -1,4 +1,4 @@
-import {cadastro, generos, signos} from "./cadastro";
+import {cadastro, generos, signos, contaSignos, contaGeneros, filtraGeneros, filtraSignos} from "./cadastro";
 
 describe('constantes', () => {
     test('comprimento do cadastro', () => {
@@ -11,5 +11,71 @@ describe('constantes', () => {
 
     test('comprimento dos generos', () => {
         expect(generos.length).toEqual(3);
+    });
+});
+
+describe('contagem', () => {
+    test('Contagem de Signos #1', () => {
+        expect(contaSignos('Virgem')).toEqual(4);
+    });
+
+    test('Contagem de Signos #2', () => {
+        expect(contaSignos('Leão')).toEqual(2);
+    });
+
+    test('Contagem de Signos #3', () => {
+        expect(contaSignos('trololo')).toEqual(0);
+    });
+
+    test('Contagem de Signos NULL', () => {
+        expect(contaSignos(null)).toEqual(0);
+    });
+
+    test('Contagem de Signos UNDEFINED', () => {
+        expect(contaSignos(undefined)).toEqual(0);
+    });
+    
+    test('Contagem de Gênero #1', () => {
+        expect(contaGeneros('Feminino')).toEqual(16);
+    });
+
+    test('Contagem de Gênero #2', () => {
+        expect(contaGeneros('Masculino')).toEqual(2);
+    });
+
+    test('Contagem de Gênero #3', () => {
+        expect(contaGeneros('Transgênero')).toEqual(1);
+    });
+
+    test('Contagem de Gênero #4', () => {
+        expect(contaGeneros('Escolha seu campeão')).toEqual(0);
+    });
+
+    test('Contagem de Gênero NULL', () => {
+        expect(contaGeneros(null)).toEqual(0);
+    });
+
+    test('Contagem de Gênero UNDEFINED', () => {
+        expect(contaGeneros(undefined)).toEqual(0);
+    });
+});
+
+describe('filtros', () => {
+    test('Filtro de Gênero #1', () => {
+        const genero = 'Feminino';
+        const resultado = filtraGeneros(genero);
+        const esperado = contaGeneros(genero);
+        expect(resultado.length).toEqual(esperado);
+        expect(resultado.map(p=>p.nome)).toEqual(expect.arrayContaining(['Laura Ribeiro','Paloma Calado']));
+        expect(resultado.map(p=>p.nome)).not.toEqual(expect.arrayContaining(['Eudes', 'Cyro Renato']));
+    });
+
+    test('Filtro de Signo #1', () => {
+        const signo = 'Virgem';
+        const resultado = filtraSignos(signo);
+        const esperado = contaSignos(signo);
+        expect(resultado.length).toEqual(esperado);
+        expect(resultado.map(p=>p.nome)).toEqual(expect.arrayContaining(['Laila Cardoso', 'Cyro Renato','Rafaela Bittencourt']));
+        expect(resultado.map(p=>p.nome)).not.toEqual(expect.arrayContaining(['Jislane Santana', 'Emilly Roberta']));
     });
 });
